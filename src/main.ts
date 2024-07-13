23e9e1d63b74bb5777a0862633626756b624fa2c
Damir Aidarbek <dameer.aidarbek@yandex.kz>
2024-07-13 13:35:19 +0500

    FIX: types

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,26 @@ import Handlebars from 'handlebars'
 import * as Components from './components'
 import * as Pages from './pages'
 
-const root = document.getElementById('app')
+const root = document.getElementById('app')!
 
 Object.entries(Components).forEach(([name, partial]) => {
     Handlebars.registerPartial(name, partial)
 })
 
-document.addEventListener('DOMContentLoaded', (e) => {
-    const pathname = e.target.location.pathname
+document.addEventListener('DOMContentLoaded', () => {
+    const pathname = window.location.pathname
     let content;
 
     switch (pathname) {
         case '/':
         case '/login':
-            content = Handlebars.compile(Pages.LoginPage)(styles)
+            content = Handlebars.compile(Pages.LoginPage)({})
             break;
         case '/register':
-            content = Handlebars.compile(Pages.RegisterPage)()
+            content = Handlebars.compile(Pages.RegisterPage)({})
             break;
         case '/profile':
-            content = Handlebars.compile(Pages.ProfilePage)()
+            content = Handlebars.compile(Pages.ProfilePage)({})
             break;
     }
 
