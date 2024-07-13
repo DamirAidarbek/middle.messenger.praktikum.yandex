0a0ff8dc5b99c0ee9cde502fbbe5f923d9956439
Damir Aidarbek <dameer.aidarbek@yandex.kz>
2024-07-13 09:48:23 +0500

    FIX: vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import handlebars from 'vite-plugin-handlebars'
 export default defineConfig({
     root: path.resolve(__dirname, 'src'),
     publicDir: path.resolve(__dirname, 'public'),
+    build: {
+        outDir: path.resolve(__dirname, 'dist')
+    },
     plugins: [
         handlebars(),
     ],
